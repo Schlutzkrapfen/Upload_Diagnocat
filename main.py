e49e59b912cbc6793061becb723ed7f05ccaacc4
Schlutzkrapfen <david.messner@protonmail.com>
2026-09-22 09:41:03 +0200

    added a login

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-
+import asyncio
+from playwright.async_api import BrowserContext, Page, async_playwright
 from webcrawler import login
 
 
-def main():
-    pass
-    #login()
+USER_DATA_DIR = "user_data"
+async def main():
+    async with async_playwright() as p:
+        context: BrowserContext = await p.chromium.launch_persistent_context(
+            USER_DATA_DIR,
+            headless=False,
+        )
+
+        page: Page = await context.new_page()
+
+
+        try:
+            await login(page)
+        except TimeoutError as e:
+            print(f"Login timed out: {e}")
 
 
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
